272e07823dd24c98a9a2c6117bd01ab77b44dea3
Arthur Ming <guobin.ming@pwc.com>
2019-07-14 17:18:01 +0800

    build: 添加初始化的province表格的数据

--- repository/db/seeds/init.ts
+++ repository/db/seeds/init.ts
@@ -1,12 +1,61 @@
 import * as Knex from 'knex';
+import { provinces } from './data/province';
 
-export const seed  = async (knex: Knex) => {
+const createTable = async (knex: Knex) => {
     await knex.raw('SET FOREIGN_KEY_CHECKS = 0');
 
     await knex.raw('DROP TABLE IF EXISTS province')
     await knex.schema.createTable('province', (table) => {
-        table.increments('id ');
+        table.increments('id');
         table.string('name');
         table.string('province_id');
     });
+
+    await knex.raw('DROP TABLE IF EXISTS city');
+    await knex.schema.createTable('city', (table) => {
+        table.increments('id');
+        table.string('name');
+        table.string('city_id');
+        table.string('province_id');
+    });
+
+    await knex.raw('DROP TABLE IF EXISTS country');
+    await knex.schema.createTable('country', (table) => {
+        table.increments('id');
+        table.string('name');
+        table.string('country_id');
+        table.string('city_id');
+    });
+
+    await knex.raw('DROP TABLE IF EXISTS town');
+    await knex.schema.createTable('town', (table) => {
+        table.increments('id');
+        table.string('name');
+        table.string('town_id');
+        table.string('country_id');
+    });
+
+    await knex.raw('DROP TABLE IF EXISTS village');
+    await knex.schema.createTable('village', (table) => {
+        table.increments('id');
+        table.string('name');
+        table.string('village_id');
+        table.string('town_id');
+    });
 }
+
+const seedProvince = async (knex: Knex) => {
+    return knex('province').insert(provinces.map(province => {
+        return {
+            name: province.name,
+            province_id: province.id
+        }
+    }));
+}
+
+export const seed = async (knex: Knex) => {
+    await createTable(knex);
+
+    await seedProvince(knex);
+}
+
--- repository/scripts/create-db.js
+++ repository/scripts/create-db.js
@@ -7,4 +7,5 @@ delete config.connection.database;
 const knex = require('knex')(config);
 
 knex.raw(`CREATE DATABASE ${database}`).finally(() => knex.destroy())
+knex.raw(`ALTER DATABASE  ${database} CHARACTER SET utf8 COLLATE utf8_general_ci;`)
 
