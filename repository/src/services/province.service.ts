fddd2f78f3a42c46ed6430e2404ee52734ce28ad
Arthur Ming <guobin.ming@pwc.com>
2019-07-19 08:49:20 +0800

    feat: add get province by id method in province service

--- repository/src/services/province.service.ts
+++ repository/src/services/province.service.ts
@@ -1,9 +1,13 @@
 import db from '../lib/db';
-import { Provinces } from '../types/province';
+import { Provinces, IProvince } from '../types/province';
 
 export const provinceService = {
 
     listProvinces(): Promise<Provinces> {
         return db.select('id', 'name').from('province');
+    },
+
+    geProvince(id: string): Promise<IProvince> {
+        return db.select('id', 'name').from('province').where('id', id).first();;
     }
 }
--- repository/test/services/province.service.test.ts
+++ repository/test/services/province.service.test.ts
@@ -2,8 +2,13 @@ import { provinceService } from '../../src/services/province.service';
 
 describe('unit test province service', () => {
     test('list provices', async () => {
-        expect.assertions(1);
         const provinces = await provinceService.listProvinces();
         return expect(provinces.length).toBe(31);
     })
+
+    test('get province by id', async() => {
+        const id = '1';
+        const province = await provinceService.geProvince(id);
+        return expect(province.name).toEqual('北京市');
+    });
 });
