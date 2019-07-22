95bf29771ac990cd21d3f3dc00e6c82becebdee3
Arthur Ming <guobin.ming@pwc.com>
2019-07-22 22:22:28 +0800

    feat(service:city): add city service

--- repository/src/services/city.service.ts
+++ repository/src/services/city.service.ts
@@ -1,4 +1,13 @@
+import db from '../lib/db';
+import { Cities, ICity } from '../types/city';
 
 export const cityService = {
-    
+
+    listCities(): Promise<Cities> {
+        return db.select('id', 'name', 'cityId', 'provinceId').from<Cities>('city');
+    },
+
+    getCity(id: string): Promise<ICity> {
+        return db.select('id', 'name', 'cityId', 'provinceId').from<Cities>('city').where('id', id).first();
+    }
 }
---
+++ repository/src/types/city.ts
@@ -0,0 +1,8 @@
+export interface ICity {
+    id: string;
+    name: string;
+    cityId: string;
+    provinceId: string;
+}
+
+export type Cities = ICity[];
