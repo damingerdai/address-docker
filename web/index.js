39d110536dedddfc83fd27fcd1efe542c5eb0e40
Arthur Ming <guobin.ming@pwc.com>
2019-07-31 22:59:13 +0800

    feat(module: web): add web api serve

---
+++ web/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var graphqlHttp = require('express-graphql');
+var { buildSchema } = require('graphql');
+
+var schema = buildSchema(`
+    type Query {
+        hello: String
+    }
+`);
+
+var root = {
+    hello: () => {
+        return 'Hello world';
+    }
+};
+
+var app = express();
+app.use('/graphql', graphqlHttp({
+    schema,
+    rootValue: root,
+    graphiql: true
+}))
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql');
