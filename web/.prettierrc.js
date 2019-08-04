35fd1a341be781f391cd3777033fbb418821c6e4
Arthur Ming <guobin.ming@pwc.com>
2019-08-04 09:44:20 +0800

    ci(web:lint): add lint support

---
+++ web/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+	printWidth: 120,
+	tabWidth: 4,
+	singleQuote: true,
+	semi: true,
+	trailingComma: 'es5',
+	bracketSpacing: true,
+	jsxBracketSameLine: true,
+	parser: 'typescript'
+};
--- web/src/server.ts
+++ web/src/server.ts
@@ -5,36 +5,38 @@ const typeDefs = gql(`
     "query api"
     type Query {
         """
-            hello
+        hello
         """
         hello: String
     }
 `);
 
 const resolvers = {
-    Query: {
-        hello: () => 'Hello World',
-    }
+	Query: {
+		hello: () => 'Hello World',
+	},
 };
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-    playground: {
-        settings: {
-            'editor.theme': 'dark',
-			'editor.cursorShape': 'line'
-        }
-    }
+	typeDefs,
+	resolvers,
+	playground: {
+		settings: {
+			'editor.theme': 'dark',
+			'editor.cursorShape': 'line',
+		},
+	},
 });
 
 const app = express();
 
 server.applyMiddleware({
-    app
+	app,
 });
 
-app.listen({
-    port: 4000
-}, () => console.log('Running a GraphQL API server at localhost:4000/graphql'))
-
+app.listen(
+	{
+		port: 4000,
+	},
+	() => console.log('Running a GraphQL API server at localhost:4000/graphql')
+);
