89201d78e40b3cf18b5a79a61ff57a6bc4f74a4e
Arthur Ming <guobin.ming@pwc.com>
2019-08-06 13:36:42 +0800

    build(web): add split chunks plugin

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -1,12 +1,18 @@
 const nodeExternals = require('webpack-node-externals');
 const TsConfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   mode: process.env.NODE_ENV === "production" ? "production" : "none",
   entry: {
     server: "./src/server.ts"
   },
-  devtool: "inline-source-map",
+	devtool: "source-map",
+	devServer: {
+		contentBase: './dist',
+		port: 4000,
+		openPage: 'graphql'
+	},
   target: "node",
   output: {
     filename: "[name].js",
@@ -19,7 +25,7 @@ module.exports = {
       'src',
     ],
 		plugins: [
-			new TsConfigPathsPlugin()
+			new TsConfigPathsPlugin(),
 		]
   },
   module: {
@@ -28,5 +34,27 @@ module.exports = {
       use: 'awesome-typescript-loader'
     }]
   },
-  externals: [nodeExternals()]
+	externals: [nodeExternals()],
+	optimization: {
+		splitChunks: {
+			chunks: 'all',
+			minSize: 30000, // 模块的最小体积
+			minChunks: 1, // 模块的最小被引用次数
+			maxAsyncRequests: 5, // 按需加载的最大并行请求数
+			maxInitialRequests: 3, // 一个入口最大并行请求数
+			automaticNameDelimiter: '~', // 文件名的连接符
+			name: true,
+			cacheGroups: { // 缓存组
+					vendors: {
+							test: /[\\/]node_modules[\\/]/,
+							priority: -10
+					},
+					default: {
+							minChunks: 2,
+							priority: -20,
+							reuseExistingChunk: true
+					}
+			}
+		}
+	}
 };
